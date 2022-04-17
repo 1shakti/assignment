8321632e8e47b187fa2198180da3546ce8f256ab
1shakti <shakti26.sj@gmail.com>
2022-04-17 13:56:37 +0530

    chess- knight moves

---
+++ chess/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ChessBoard from "./container/ChessBoard/ChessBoard"
+function App() {
+  return (
+    <div className="App">
+      <ChessBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ chess/src/Const/Board.js
@@ -0,0 +1,24 @@
+export const column = ["a", "b", "c", "d", "e", "f", "g", "h"];
+export const row = [8, 7, 6, 5, 4, 3, 2, 1];
+
+
+export const Whitepiece = [
+    {
+        "name": "Knight",
+        "img": "knight_w.png",
+        "length": 2,
+        "alias": "wn",
+        "position": { "letter": ["b", "g"], "number": 1 }
+    }
+];
+
+
+export const BlackPiece = [
+    {
+        "name": "Knight",
+        "img": "knight_w.png",
+        "length": 2,
+        "alias": "bn",
+        "position": { "letter": ["b", "g"], "number": 8 }
+    }
+]
